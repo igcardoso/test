2fe5bb0a81527f3c696f84fd8c305d0ec322d565
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-08 21:05:34 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //TMDB 
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=03c4e3dc470296959d6bf68804146538';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const IMG_URL = 'https://image.tmdb.org/t/p/original';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 const genres = [
